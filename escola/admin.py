30a2f05c2b5c853a28ce6119c08ee30823043abd
Breno Rodrigues <brenocartoon@hotmail.com>
2021-12-21 14:21:03 -0300

    Register aluno and curso admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
-# Register your models here.
+class Alunos(admin.ModelAdmin):
+  list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+  list_display_links = ('id', 'nome')
+  search_field = ('nome',)
+  list_per_page = 20
+
+class Cursos(admin.ModelAdmin):
+  list_display = ('id', 'codigo_curso', 'descricao')
+  list_display_links = ('id', 'codigo_curso')
+  search_field = ('codigo_curso',)
+  list_per_page = 20
+
+admin.site.register(Aluno, Alunos)
+admin.site.register(Curso, Cursos)
