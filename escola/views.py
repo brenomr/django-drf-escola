d3ccc2d9a0ce9e8f173afb339a450953480a2f4e
Breno Rodrigues <brenocartoon@hotmail.com>
2021-12-21 16:07:50 -0300

    Updating aluno and curso viewset

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,13 @@
-from django.http import JsonResponse
+from rest_framework import serializers, viewsets
+from escola.models import Aluno, Curso
+from serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-  if request.method == 'GET':
-    aluno = { "id": 1, "nome": "Rafael Silva" }
-    return JsonResponse(aluno)
+class AlunosViewset(viewsets.ModelViewSet):
+  """Exibir alunos"""
+  queryset = Aluno.objects.all()
+  serializer_class = AlunoSerializer
+
+class CursosViewset(viewsets.ModelViewSet):
+  """Exibir cursos"""
+  queryset = Curso.objects.all()
+  serializers_class = CursoSerializer
