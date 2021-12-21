7549b9081722afe1a7b2840a6666c764242aaf38
Breno Rodrigues <brenocartoon@hotmail.com>
2021-12-21 14:00:46 -0300

    Register rest framework and url

--- setup/urls.py
+++ setup/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 from escola.views import alunos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos)
+    path('alunos/', alunos),
+    path('api-auth/', include('rest_framework.urls')),
 ]
