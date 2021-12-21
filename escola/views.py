43c0dc3791588591c39385e98ed3837c6172ff6d
Breno Rodrigues <brenocartoon@hotmail.com>
2021-12-21 17:44:10 -0300

    Create matricula and lista matricula views

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from escola.serializer import AlunoSerializer, CursoSerializer
+from rest_framework import generics, serializers, viewsets
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, ListaMatriculasAlunoSerializer, MatriculaSerializer
 
 class AlunosViewset(viewsets.ModelViewSet):
   """Exibir alunos"""
@@ -11,3 +11,16 @@ class CursosViewset(viewsets.ModelViewSet):
   """Exibir cursos"""
   queryset = Curso.objects.all()
   serializer_class = CursoSerializer
+
+class MatriculasViewset(viewsets.ModelViewSet):
+  """Exibir matrículas"""
+  queryset = Matricula.objects.all()
+  serializer_class = MatriculaSerializer
+
+class ListaMatriculasAlunoViewset(generics.ListAPIView):
+  """Exibir as matrículas por aluno"""
+  serializer_class = ListaMatriculasAlunoSerializer
+
+  def get_queryset(self):
+    queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+    return queryset
